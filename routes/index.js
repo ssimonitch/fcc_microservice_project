83bbb2aec15e9991c936dcf765b90e05aba7fa89
System <none>
2017-07-18 03:23:44 +0000

    Checkpoint

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 const controller = require('../controllers');
 
 module.exports = (app) => {
-  app.get('/', (req, res) => {
+  
+  app.get('/', (request, response) => {
+    response.sendFile('index.html', {root: __dirname + '/../views'});
+  });
+  
+  app.get('/api', (req, res) => {
     res.status(200).send({
       message: 'API test'
     });
   });
   
-  
+  app.get('*', (req, res) => res.status(200).send({
+  message: 'Nothing here.'
+}))
 }
--- server.js
+++ server.js
@@ -3,25 +3,15 @@ const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
+const router = require('./routes/');
 
 // middleware
 app.use(morgan('combined'));
 app.use(express.static('public'));
 app.use(bodyParser.json({ type: '*/*'}));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + '/views/index.html');
-});
-
-app.get("/", (request, response) => {
-  response.sendStatus(200);
-});
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/", (request, response) => {
-  response.sendStatus(200).send();
-});
+// routes
+router(app);
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
