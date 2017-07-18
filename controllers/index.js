e5d4863bc0c7580470b121f4f7d26aa556dea816
System <none>
2017-07-18 03:56:43 +0000

    Checkpoint

--- controllers/index.js
+++ controllers/index.js
@@ -9,15 +9,23 @@ module.exports = {
       return res.status(422).send({ message: 'Must provide date'});
     }
     
-    const unix = moment.unix(date);
-    const natural = moment(date).format('MMM Do, ')
-    
-    if (moment.unix(date).isValid()) {
-      return res.status(200).send({ message: 'Valid timestamp' });
+    if (moment.unix(date).isValid() || ) {
+      const unix = moment(date).format('X');
+      const natural = moment.unix(date).format('MMM D, YYYY');
+      
+      return res.status(200).send({ unix, natural });
     } else if (moment(date).isValid()) {
-      return res.status(200).send({ message: 'Valid date' });
+      const unix = moment(date).unix();
+      const natural = moment(date).format('MMM D, YYYY');
+      
+      return res.status(200).send({
+        unix,
+        natural
+      });
     } else {
-      return res.status(422).send({ message: 'Not a valid date format'});
+      return res.status(422).send({ 
+        message: `${date} is not a valid date format`
+      });
     }
   }
 }
