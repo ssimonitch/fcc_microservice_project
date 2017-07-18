712c35159d439ada13df69d3ee7e7638a3cd2e86
System <none>
2017-07-18 04:40:44 +0000

    Checkpoint

--- controllers/index.js
+++ controllers/index.js
@@ -4,14 +4,16 @@ module.exports = {
   
   timestamp(req, res) {
     // grab date from body if post or default to pathname for get
-    const date = req.body.date || window.location.pathname.split('/')[1];
+    const date = req.body.date;
+    
+    return res.status(200).send({ message: res.headers})
     
     if (!date) {
       return res.status(422).send({ message: 'Must provide date'});
     }
     
     if (moment.unix(date).isValid()) {
-      const unix = moment(date).format('X');
+      const unix = moment.unix(date).format('X');
       const natural = moment.unix(date).format('MMM D, YYYY');
       
       return res.status(200).send({ unix, natural});
